c72f2454acbb104d2b4ded047d4112bbbbfea44e
Daade Live TV <daade@live-tv.de>
2026-06-14 16:08:28 +0200

    🎨 Farbige Linien + Y-Formation + Yazeed Shows Text → 1Shows

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,20 +4,18 @@ import LoadingAnimation from '../components/LoadingAnimation'
 export default function Home() {
   const [done, setDone] = useState(false)
 
-  // Redirect sobald Animation fertig ist
   useEffect(() => {
     if (done) {
       window.location.replace('https://www.1shows.org')
     }
   }, [done])
 
-  // Sicherheitsfallback: nach 5 Sekunden weiterleiten, falls Animation hängt
   useEffect(() => {
     const timer = setTimeout(() => {
       if (!done) {
         window.location.replace('https://www.1shows.org')
       }
-    }, 5000)
+    }, 7000)
     return () => clearTimeout(timer)
   }, [done])
 
